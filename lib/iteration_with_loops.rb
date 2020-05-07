2ae3d96a1635bf9c517fc09665e6be5edaabb4d2
Rene Avila <reneavila1993@gmail.com>
2020-05-07 20:12:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,20 +1,3 @@
-# def join_nested_strings(src)
-#   outer_string = []
-#   row_index = 0 
-#   while row_index < src.length do 
-#     element_index = 0 
-#     inner_string = []
-#     while element_index < src[row_index].length do 
-#       if src[row_index][element_index].is_a? String
-#         inner_string << src[row_index][element_index]
-#       end 
-#       element_index += 1 
-#     end
-#     row_index += 1
-#     outer_string << inner_string
-#   end
-#   return outer_string.join(' ')
-# end
 
 def join_nested_strings(src)
   array = []
