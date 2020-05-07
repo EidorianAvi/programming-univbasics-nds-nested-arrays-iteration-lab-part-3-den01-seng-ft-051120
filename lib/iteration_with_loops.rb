0aeae1a501d3bd4b73b3eecd3bba8062cc67779a
Rene Avila <reneavila1993@gmail.com>
2020-05-07 20:10:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -20,13 +20,15 @@ def join_nested_strings(src)
   array = []
   row_index = 0 
   while row_index < src.length do 
+    inner_array = []
     element_index = 0 
     while element_index < src[row_index].length do 
       if src[row_index][element_index] == String
-        array << src[row_index][element_index]
+        inner_array << src[row_index][element_index]
       end
       element_index += 1 
     end
+    array << inner_array
     row_index += 1 
   end
   array.join(" ")
